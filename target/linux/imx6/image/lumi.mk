caa2c470dd410530abfc94eadefd86c3e0b43e78
Ivan Belokobylskiy <belokobylskij@gmail.com>
2021-03-09 23:39:06 +0300

    Revert Aqara device because no driver present

--- target/linux/imx6/image/lumi.mk
+++ target/linux/imx6/image/lumi.mk
@@ -20,25 +20,3 @@ define Device/lumi
 	imx6ull-xiaomi-lumi
 endef
 TARGET_DEVICES += lumi
-
-define Device/aqara
-  DEVICE_VENDOR := Xiaomi
-  DEVICE_MODEL := Lumi Aqara Gateway
-  DEVICE_VARIANT := ZHWG11LM
-  DEVICE_NAME := xiaomi,gateway-aqara
-  DEVICE_PACKAGES := kmod-rtl8189es
-
-  KERNEL_INSTALL := 0
-  KERNEL_NAME := zImage
-  KERNEL_SUFFIX := -zImage
-  KERNEL := kernel-bin
-  IMAGES := sysupgrade.bin dtb rootfs.bin
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-  IMAGE/rootfs.bin := append-rootfs
-  IMAGE/dtb := install-dtb
-
-  DEVICE_DTS:= \
-	imx6ull-xiaomi-lumi
-endef
-TARGET_DEVICES += aqara
-
