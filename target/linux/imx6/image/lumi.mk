636e2dcd902898878129a5a5a01a4c53e8c541aa
Ivan Belokobylskiy <belokobylskij@gmail.com>
2021-03-09 23:56:50 +0300

    Forced kernel cmd

--- target/linux/imx6/image/lumi.mk
+++ target/linux/imx6/image/lumi.mk
@@ -5,7 +5,11 @@ define Device/lumi
   DEVICE_MODEL := Lumi Gateway
   DEVICE_VARIANT := DGNWG05LM
   DEVICE_NAME := xiaomi,gateway-lumi
-  DEVICE_PACKAGES := kmod-rtl8723bs
+  DEVICE_PACKAGES := kmod-rtl8723bs rtl8723bs-firmware kmod-bluetooth \
+  	kmod-sound-core kmod-button-hotplug kmod-bluetooth \
+  	bluez-daemon bluez-libs bluez-utils alsa-utils hostapd \
+  	wpa-supplicant ca-certificates kmod-gpio-button-hotplug \
+  	kmod-i2c-core kmod-iio-core
 
   KERNEL_INSTALL := 0
   KERNEL_NAME := zImage
